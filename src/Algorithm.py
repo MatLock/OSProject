83724d0230c7261f274a95c075a6fe85aefeb9c4
MatLock <jfflores90@gmail.com>
2013-09-29 00:02:36 -0300

    modified algorithm and thread

--- src/Algorithm.py
+++ src/Algorithm.py
@@ -7,33 +7,31 @@ Created on 23/09/2013
 from Thread import *
 from Queue import Queue
 
+
 class FIFO(threading.Thread):
-    
+
     def __init__(self):
         threading.Thread.__init__(self)
         self.queue = Queue()
-        
-    def add(self,thread):
+
+    def add(self, thread):
         self.queue.put(thread)
-        
+
     def get(self):
         return self.queue.get()
-        
+
     def execute(self):
         self.get().start()
         semaphore.acquire()
-        
+
     def run(self):
         while (True):
             self.execute()
-            
-    
-            
-        
-        
+
+
 def main():
-    x = Thread(1,'a',3)
-    y = Thread(2,'b',4)
+    x = Thread(1, 'a', 3)
+    y = Thread(2, 'b', 4)
     algorithm = FIFO()
     algorithm.add(x)
     algorithm.add(y)
--- src/Thread.py
+++ src/Thread.py
@@ -9,24 +9,25 @@ import time
 
 semaphore = threading.Semaphore(0)
 
+
 class Thread(threading.Thread):
-    
+
     def __init__(self, num, pid, size):
         threading.Thread.__init__(self)
         self.priority = num
         self.id = pid
         self.size = size
-        
+
     def getPriority(self):
         return self.priority
-        
-    def execute(self,i):
+
+    def execute(self, i):
         print "I'm the thread : " + str(self.id)+ "  executing instruction number: " + str(i) + "  of  " + str(self.size)
         time.sleep(2)
-             
+
     def run(self):
         i = 1
         while (i <= self.size):
             self.execute(i)
-            i+= 1
+            i += 1
         semaphore.release()
